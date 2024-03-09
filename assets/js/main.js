2758d91e04cce766c88f020f390c75e203634144
Vinicius_Vidal <viniciusvrdsilva@gmail.com>
2024-03-09 01:13:55 -0300

    site criado

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+function scrollToEnredo() {
+    let element = document.getElementById("enredo");
+    element.scrollIntoView();
+}
+
+function scrollToElenco(){
+    let element = document.getElementById("elenco");
+    element.scrollIntoView();
+}
+
+function scrollToElencoPrincipal(){
+    let element = document.getElementById("elencoPrincipal");
+    element.scrollIntoView();
+}
+
+function scrollToComercial(){
+    let element = document.getElementById("comercial");
+    element.scrollIntoView();
+}
+
+function scrollToCritica(){
+    let element = document.getElementById("critica");
+    element.scrollIntoView();
+}
+
+function scrollToDinossauros(){
+    let element = document.getElementById("dinossauros");
+    element.scrollIntoView();
+}
+
